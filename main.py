e14da20d7dc36b48466218546673ab24c04950f2
kantanana <85265596+kantanana@users.noreply.github.com>
2023-10-11 14:23:00 +0000

    working word counter for excluding citations

---
+++ main.py
@@ -0,0 +1,23 @@
+wordcount = 0
+apa = False
+flag = False
+with open('input.txt', 'r') as f:
+    for para in f:
+        wordcount += 1
+        para = para.strip()
+        for each in para:
+            if flag:
+                if each == ",":
+                    apa = True
+                if each == ")" and apa:
+                    flag = False
+                    wordcount -= 1
+                if each == ")":
+                    flag = False
+            else:
+                if each == " ":
+                    wordcount += 1
+                elif each == "(":
+                    flag = True
+
+print(f'word count: {wordcount}')
